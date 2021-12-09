ea0b8ba27a6523ed2cfd0ca67b8e49cfb8e69f65
Andronick Martusheff <andronick@outlook.com>
2021-12-08 20:12:15 -0800

    Updated current proj for Pages Deployement

--- script.js
+++ script.js
@@ -1,61 +1,9 @@
-const panels = document.querySelectorAll('.panel')
-const nextPhoto = document.querySelector('.nextPhoto')
-const lastPhoto = document.querySelector('.lastPhoto')
-const clearSelection = document.querySelector('.clearSelection')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+open.addEventListener('click', () => container.classList.add
+('show-nav'))
 
-function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove('active')
-    })
-}
-
-nextPhoto.addEventListener('click', () => {
-    selectNextPhoto()
-})
-
-lastPhoto.addEventListener('click', () => {
-    selectLastPhoto()
-})
-
-clearSelection.addEventListener('click', () => {
-    removeActiveClasses()
-})
-
-function selectLastPhoto() {
-    var currentlySelected = getSelectedPhotoIndex()
-
-    if(currentlySelected > 0 && currentlySelected <= 4) {
-        removeActiveClasses()
-        panels[currentlySelected - 1].classList.add('active')
-    } else {
-        removeActiveClasses()
-        panels[4].classList.add('active')
-    }
-}
-
-function selectNextPhoto() {
-    var currentlySelected = getSelectedPhotoIndex()
-    
-    if(currentlySelected >= 0 && currentlySelected < 4) {
-        removeActiveClasses()
-        panels[currentlySelected + 1].classList.add('active')
-    } else {
-        removeActiveClasses()
-        panels[0].classList.add('active')
-    }
-}
-
-function getSelectedPhotoIndex() {
-    for(var i = 0; i < panels.length; i++) {
-        if(panels[i].classList.contains('active')) {
-            return i;
-        }
-    }
-}
+close.addEventListener('click', () => container.classList.remove
+('show-nav'))
