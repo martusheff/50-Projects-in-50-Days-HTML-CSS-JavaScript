27c11566417b36b037870eb169b65958445ff593
Andronick Martusheff <andronick@outlook.com>
2021-12-08 23:39:17 -0800

    Updated w/ adding color to fuces list item in navigation

--- Rotating-Navigation/script.js
+++ Rotating-Navigation/script.js
@@ -2,8 +2,22 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
+const home = document.getElementById('home')
+const about = document.querySelector('.fa-user-alt')
+const contact = document.querySelector('.fa-envelope')
+
+const homeContent = document.getElementById('home-content')
+
+var counter = 0
+
+
 open.addEventListener('click', () => container.classList.add
 ('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove
 ('show-nav'))
+
+home.addEventListener('click', () => {
+    counter++
+    console.log(counter)
+})
