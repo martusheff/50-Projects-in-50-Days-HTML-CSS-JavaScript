3ec1f1d2067a55be4711c31e90101e724b5ac6ee
Andronick <contact@andronickmartusheff.com>
2021-12-31 00:55:59 -0800

    update deployed site

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const insert = document.querySelector('.insert')
+const input = document.getElementById('quantity')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const addToCart = document.getElementById('addToCart')
+const confirmation = document.getElementById('confirmation')
+const toast = document.querySelector('.toast')
 
+var valueInt = parseInt(input.value)
 
-window.addEventListener('keydown', (event) => {
-    insert.innerHTML = `
-        <div class="key">
-            ${event.key === ' ' ? 'Space' : event.key}
-            <small>event.key</small>
-        </div>
-        
-        <div class="key">
-            ${event.keyCode}
-            <small>event.keyCode</small>
-        </div>
-
-        <div class="key">
-            ${event.code}
-            <small>event.code</small>
-        </div>
-    `
+plus.addEventListener('click', () => {
+    confirmation.classList.remove('active')
+    valueInt = parseInt(input.value)
+    if(valueInt + 1 > 99) {
+        valueInt = 99
+    } else {
+        ++valueInt
+    }
+    input.value = valueInt
 })
+
+minus.addEventListener('click', () => {
+    confirmation.classList.remove('active')
+    valueInt = parseInt(input.value)
+    if(valueInt - 1 < 1) {
+        valueInt = 1
+    } else {
+        --valueInt
+    }
+    input.value = valueInt
+
+})
+
+addToCart.addEventListener('click', () => {
+    showToast()
+    valueInt = parseInt(input.value)
+    confirmation.classList.add('active')
+    confirmation.innerHTML = "Added <b>" + valueInt + " item(s) </b>to your cart."
+    valueInt = 1
+    input.value = 1
+    setTimeout(dropToast, 3000)
+})
+
+function showToast() {
+    toast.classList.add('active')
+}
+
+function dropToast() {
+    toast.classList.remove('active')
+}
